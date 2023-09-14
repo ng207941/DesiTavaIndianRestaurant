26b1daa9e7d832551e8f90bbdffec60a7d86e4c4
Nayan65896 <nayan65896@gmail.com>
2023-09-14 14:04:17 -0600

    category with details

--- src/DesiTava/AppBar/FullSizeMenu/ChildMenuOption/ChildMenuOption.js
+++ src/DesiTava/AppBar/FullSizeMenu/ChildMenuOption/ChildMenuOption.js
@@ -1,89 +1,30 @@
 import * as React from 'react';
-import PropTypes from 'prop-types';
-import { useTheme } from '@mui/material/styles';
-import AppBar from '@mui/material/AppBar';
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
-import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import SwipeableViews from "react-swipeable-views/src";
-
-function TabPanel(props) {
-    const { children, value, index, ...other } = props;
-
-    return (
-        <div
-            role="tabpanel"
-            hidden={value !== index}
-            id={`full-width-tabpanel-${index}`}
-            aria-labelledby={`full-width-tab-${index}`}
-            {...other}
-        >
-            {value === index && (
-                <Box sx={{ p: 3 }}>
-                    <Typography>{children}</Typography>
-                </Box>
-            )}
-        </div>
-    );
-}
-
-TabPanel.propTypes = {
-    children: PropTypes.node,
-    index: PropTypes.number.isRequired,
-    value: PropTypes.number.isRequired,
-};
-
-function a11yProps(index) {
-    return {
-        id: `full-width-tab-${index}`,
-        'aria-controls': `full-width-tabpanel-${index}`,
-    };
-}
+import Tab from '@mui/material/Tab';
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import TabPanel from '@mui/lab/TabPanel';
+import {VegMenu} from "./VegMenu/VegMenu";
 
 export function ChildMenuOption() {
-    const theme = useTheme();
-    const [value, setValue] = React.useState(0);
+    const [value, setValue] = React.useState('1');
 
     const handleChange = (event, newValue) => {
         setValue(newValue);
     };
 
-    const handleChangeIndex = (index) => {
-        setValue(index);
-    };
-
     return (
-        <Box sx={{ bgcolor: 'background.paper', width: 500 }}>
-            <AppBar position="static">
-                <Tabs
-                    value={value}
-                    onChange={handleChange}
-                    indicatorColor="secondary"
-                    textColor="inherit"
-                    variant="fullWidth"
-                    aria-label="full width tabs example"
-                >
-                    <Tab label="Item One" {...a11yProps(0)} />
-                    <Tab label="Item Two" {...a11yProps(1)} />
-                    <Tab label="Item Three" {...a11yProps(2)} />
-                </Tabs>
-            </AppBar>
-            <SwipeableViews
-                axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
-                index={value}
-                onChangeIndex={handleChangeIndex}
-            >
-                <TabPanel value={value} index={0} dir={theme.direction}>
-                    Item One
-                </TabPanel>
-                <TabPanel value={value} index={1} dir={theme.direction}>
-                    Item Two
-                </TabPanel>
-                <TabPanel value={value} index={2} dir={theme.direction}>
-                    Item Three
-                </TabPanel>
-            </SwipeableViews>
+        <Box sx={{ width: '100%', typography: 'body1' }}>
+            <TabContext value={value}>
+                <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                    <TabList onChange={handleChange} aria-label="lab API tabs example">
+                        <Tab label="Veg" value="1" />
+                        <Tab label="Non-veg" value="2" />
+                    </TabList>
+                </Box>
+                <TabPanel value="1"><VegMenu/></TabPanel>
+                <TabPanel value="2"><VegMenu/></TabPanel>
+            </TabContext>
         </Box>
     );
 }
---
+++ src/DesiTava/AppBar/FullSizeMenu/ChildMenuOption/VegMenu/VegMenu.js
@@ -0,0 +1,111 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Avatar from '@mui/material/Avatar';
+import IconButton from '@mui/material/IconButton';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import FolderIcon from '@mui/icons-material/Folder';
+import DeleteIcon from '@mui/icons-material/Delete';
+
+function generate(element) {
+    return [0].map((value) =>
+        React.cloneElement(element, {
+            key: value,
+        }),
+    );
+}
+
+const Demo = styled('div')(({ theme }) => ({
+    backgroundColor: theme.palette.background.paper,
+}));
+
+const names = [ {Item: 'Recent Orders', detail: 'order just placed'},
+    {Item: 'Appetizer', detail: 'a small dish of food or a drink taken before a meal or the main course of a meal'},
+    {Item: 'Soups', detail: 'Soup is a primarily liquid food, generally served warm or hot that is made by combining ingredients of meat or vegetables with stock, milk, or water'},
+    {Item: 'Dinner', detail: 'Indian meals comprise of rice, Chapati (flatbread), meat, vegetable and lentil dishes, salad, yogurt, and pickles from 4 to 9 pm'},
+    {Item: 'Lunch', detail: 'Indian meals comprise of rice, Chapati (flatbread), meat, vegetable and lentil dishes, salad, yogurt, and pickles from 12 to 4 pm'},
+    {Item: 'Breakfast', detail: 'Indian meals comprise of rice, Chapati (flatbread), meat, vegetable and lentil dishes, salad, yogurt, and pickles from 10AM to 12PM'},
+    {Item: 'Drink', detail: 'Hot Indian Drinks'}
+];
+
+export function VegMenu() {
+    const [dense, setDense] = React.useState(false);
+    const [secondary, setSecondary] = React.useState(false);
+
+    return (
+        <Box sx={{ flexGrow: 1, maxWidth: 752 }}>
+            <FormGroup row>
+                <FormControlLabel
+                    control={
+                        <Checkbox
+                            checked={dense}
+                            onChange={(event) => setDense(event.target.checked)}
+                        />
+                    }
+                    label="Enable dense"
+                />
+                <FormControlLabel
+                    control={
+                        <Checkbox
+                            checked={secondary}
+                            onChange={(event) => setSecondary(event.target.checked)}
+                        />
+                    }
+                    label="Click to see more details of the category"
+                />
+            </FormGroup>
+            <Grid container spacing={4}>
+                {/*<Grid item xs={12} md={6}>*/}
+                {/*    <Typography sx={{ mt: 4, mb: 2 }} variant="h6" component="div">*/}
+                {/*        Text only*/}
+                {/*    </Typography>*/}
+                {/*    <Demo>*/}
+                {/*        <List dense={dense}>*/}
+                {/*            {names.map((name) => (*/}
+                {/*                generate(*/}
+                {/*                    <ListItem>*/}
+                {/*                        <ListItemText*/}
+                {/*                            primary= {name}*/}
+                {/*                            secondary={secondary ? 'Secondary text' : null}*/}
+                {/*                        />*/}
+                {/*                    </ListItem>,*/}
+                {/*                )*/}
+                {/*            ))}*/}
+                {/*        </List>*/}
+                {/*    </Demo>*/}
+                {/*</Grid>*/}
+                <Grid item xl={2} md='auto'>
+                    <Typography sx={{ mt: 4, mb: 2 }} variant="h6" component="div">
+                        BROWSE BY CATEGORY
+                    </Typography>
+                    <Demo>
+                        <List dense={dense}>
+                            {names.map((name) => (
+                                generate(
+                                    <ListItem>
+                                        <ListItemIcon>
+                                            <FolderIcon />
+                                        </ListItemIcon>
+                                        <ListItemText
+                                            primary= {name.Item}
+                                            secondary={secondary ? name.detail : null}
+                                        />
+                                    </ListItem>,
+                                )
+                            ))}
+                        </List>
+                    </Demo>
+                </Grid>
+            </Grid>
+        </Box>
+    );
+}
